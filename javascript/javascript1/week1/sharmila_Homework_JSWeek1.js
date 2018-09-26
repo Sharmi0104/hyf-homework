436da83b6094a8a9925e483ceef80a27a327f7d8
Sharmila pannyam <i.sp@leiairship.onmicrosoft.com>
2018-09-26 11:16:33 +0200

    Added javascript1 week1 homework

---
+++ javascript/javascript1/week1/sharmila_Homework_JSWeek1.js
@@ -0,0 +1,42 @@
+//Write a console.log statement saying "Hello World!" for each language that you know.
+console.log("Hello world");
+console.log("வணக்கம்");
+//Whole number
+let wholeNo ;
+wholeNo= Math.round(2.5);
+
+console.log(wholeNo);
+
+//Arrray
+
+let emptyArray =[];
+console.log("This is empty array:" +emptyArray);
+
+//Favorite animals
+let favAnimal=["Dog","Cat","Elephant","Monkey","Bear"];
+console.log(favAnimal);
+
+favAnimal.push("baby pig");
+console.log(favAnimal);
+
+//Length of string
+let newStr ="this is a test";
+console.log(newStr);
+let strLength=newStr.length;
+console.log(strLength);
+//variable types
+let newstring ="First String";
+console.log('The value of my variable newstring is: '+newstring);
+const newNo =20;
+console.log('The value of my variable newNo is: '+newNo);
+let newBoolean=true;
+console.log('The value of my variable newBoolean is: '+newBoolean);
+let noValue =null;
+console.log('The value of my variable noValue is: '+noValue);
+
+//typeof
+let strTypeOf ="This is  string typeOf :";
+console.log(strTypeOf + typeof(strTypeOf));
+
+let noTypeOf ="This is  number typeOf :";
+console.log(noTypeOf + typeof(noTypeOf));
