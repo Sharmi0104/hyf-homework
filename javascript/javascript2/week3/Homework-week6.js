d870a5b31cc64e80f068b9a6143f88baa637afb9
Sharmila pannyam <i.sp@leiairship.onmicrosoft.com>
2018-11-02 16:26:33 +0100

    Homework-Week6

---
+++ javascript/javascript2/week3/Homework-week6.js
@@ -0,0 +1,63 @@
+var titleDiv=document.getElementById("divName");
+document.getElementById("btnRepo").addEventListener("click",getJson)
+
+function getJson()
+{
+
+fetch('https://api.github.com/orgs/HackYourFuture-CPH/repos')
+.then((res)=>{return res.json()})
+
+.then((data) => {
+    titleDiv.hidden=false;
+    data.forEach(function (x){
+        titleDiv.innerHTML += `<br> <ul>
+      <li>          
+      <button class='btntittle' ">${x.name}</button>
+      </li>
+      </ul>`;    
+  }) 
+})
+}
+var divRepo=document.getElementById("divRepo");
+var divRepoUrl=document.getElementById("divRepoUrl");
+document.getElementById("btnSearch").addEventListener("click",searchRepo)
+
+function searchRepo()
+{
+  let SearchText=document.getElementById("txtReop").value;
+  if(SearchText!=''){
+  fetch(`https://api.github.com/search/repositories?q=user:HackYourFuture+${SearchText}`)
+.then((res)=>{return res.json()})
+
+.then((data) => {
+ 
+  data.items.map(function(x){
+        titleDiv.hidden=true;
+    divRepo.innerHTML += `<br>
+    <div class="alignrow">
+    <div class="aligncol">
+     <ul>
+      <li>          
+      <a href='https://api.github.com/repos/HackYourFuture/${x.name}' target="_blank">${x.name}</a>
+      </li>
+      </ul>
+      </div>
+      <div class="aligncol" >
+      <ul>
+      <li> 
+      <a href='https://api.github.com/repos/HackYourFuture/${x.contributors_url}' target="_blank">${x.contributors_url}</a>
+      </li>
+      </ul>
+      </div>
+      </div>
+      `; 
+      
+  }).join('');
+})
+}
+else{
+
+  alert('Please enter search text');
+}
+
+}
